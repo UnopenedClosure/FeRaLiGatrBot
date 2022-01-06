a8b39a373ca3796d2cf7e814ffb27c0d109cc074
unope <unope@LAPTOP-4IMDQARE>
2022-01-05 20:36:59 -0500

    create Route.lua file for Yellow TID manip

--- Route.lua
+++ Route.lua
@@ -4,18 +4,18 @@
 
 Route = {
   startFrame = 530,
-  totalMaxFrames = 420,
+  totalMaxFrames = 380,
 --  totalMaxFrames = 9500, -- this is the total frames allowed for meeting all subgoals
 --allow 255 frames for first movement inside PC
 
 -- Subgoals are achieved from first to last
   subgoals = {
     Subgoal.waitFrames(4),
-    Subgoal.pressButton("Start", 3),
+    Subgoal.pressButton("Start", 15),
     Subgoal.waitFrames(60),
-    Subgoal.pressButton("A", 3),
+    Subgoal.pressButton("A", 15),
     Subgoal.waitFrames(156),
-    Subgoal.pressButton("Start", 3),
+    Subgoal.pressButton("Start", 15),
     Subgoal.waitFrames(109),
     {
       name = "set Trainer ID",
@@ -24,11 +24,11 @@ Route = {
           register = 0xD358,
           numBytes = 2,
           bigEndianFlag = true,
-          expectedValue = {30753, 41980}
+          expectedValue = {0x64CF, 0x64D0, 0x64EA}
           --expectedValue = 30753
         }
       },
-      numFrames = 35,
+      numFrames = 31 + 15,
       permittedInputs = {"A,NO_INPUT,NO_INPUT," ..
                          "NO_INPUT,NO_INPUT,NO_INPUT," ..
                          "NO_INPUT,NO_INPUT,NO_INPUT," ..
